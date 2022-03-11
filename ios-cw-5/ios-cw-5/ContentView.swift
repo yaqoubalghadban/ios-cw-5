4795cd051a5ea6fb4e023e67663037d4640d258b
yaqoubalghadban <yoyob4654@gmail.com>
2022-03-11 20:08:32 +0300

    done

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,11 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack{
+            HStack{
+                    
+                Image("haha")
+                    .resizable()
+                    .frame(width: 120, height: 120 )
+                    .scaledToFit()
+                    .clipShape(Circle())
+                
+                
+                Text("ios memes")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.red)
+                    .padding()
+                
+            }.padding()//hstack1
+            HStack{
+            Image("image1")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .scaledToFit()
+            Image("image2")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .scaledToFit()
+                Image("image3")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .scaledToFit()            }//hstack2
+            HStack{
+            Image("image4")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .scaledToFit()
+            Image("image5")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .scaledToFit()
+                Image("image6")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .scaledToFit()            }//hstack3
+            Spacer()
+        }
 }
-
+}//vstack1
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
